60796efc03b4e2f02d7cab3fa019f6412fbae1f6
Tse Lun Bien <allanbian@gmail.com>
2019-05-25 19:14:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='mbpprice',
-    version='0.0.1',
+    version='0.0.2',
     description="second hand macbook pro price in Taiwan.",
     long_description=long_description,
     long_description_content_type="text/markdown",
